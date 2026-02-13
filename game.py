239278a40fcae35d53196e061e2d62be356f8138
gjosborne1 <gjosborne81@1770.us>
2026-02-13 13:45:44 -0500

    minor formatting changes

--- game.py
+++ game.py
@@ -4,11 +4,12 @@ import random
 balance = 100.0
 inp = None
 clear()
-print("""Hello. Place a bet?
+print("""Hello, and welcome to roulette. Would you like to place a bet?
 ---
 Type "bet" to start betting
 Type "balance" to view current money amount
 Type "help" for more info on how betting works
+Type "save" to create, save, or restore data to your computer
 Type "quit" to quit
 ---""")
 while True:
@@ -109,7 +110,7 @@ while True:
                         print("Invalid input, please type \"r\" or \"b\" or move on by typing \"n\"")
             inp=None
             while inp!="1" and inp!="2" and inp!="3" and inp!="n":
-                inp = input("Type \"1\" to bet on row 1, \"2\" to bet on row 2, or \"3\" to bet on row 3\n").lower()
+                inp = input("Type \"1\" to bet on column 1, \"2\" to bet on column 2, or \"3\" to bet on column 3\n").lower()
                 match inp:
                     case "1":
                         row1=True
@@ -123,7 +124,7 @@ while True:
                         print("Invalid input, please type \"1\" or \"2\" or \"3\" or move on by typing \"n\"")
             inp=None
             while inp!="1" and inp!="2" and inp!="n":
-                inp=input("Type \"1\" to bet on half 1 or \"2\" to bet on half 2\n").lower()
+                inp=input("Type \"1\" to bet on half 1 (1-18) or \"2\" to bet on half 2 (19-36)\n").lower()
                 match inp:
                     case "1":
                         half1=True
@@ -135,7 +136,7 @@ while True:
                         print("Invalid input, please type \"1\" or \"2\" or move on by typing \"n\"")
             inp=None
             while inp!="1" and inp!="2" and inp!="3" and inp!="n":
-                inp = input("Type \"1\" to bet on third 1, \"2\" to bet on third 2, or \"3\" to bet on third 3\n").lower()
+                inp = input("Type \"1\" to bet on third 1 (1st dozen), \"2\" to bet on third 2 (2nd dozen), or \"3\" to bet on third 3 (3rd dozen)\n").lower()
                 match inp:
                     case "1":
                         third1=True
@@ -167,15 +168,17 @@ while True:
                         pass
                     case _:
                         print("Invalid input, please type \"y\" or move on by typing \"n\"")
-            #Make straight-up bet here
+            inp=None
+            #Make single bet here
             load()
             print(f"Landed on: {num_actual}")
             print(f"{num_actual} is {odd_even}")
             print(f"{num_actual} is {color}")
             if row!=0:
-                print(f"{num_actual} is in row {row}")
+                print(f"{num_actual} is in column {row}")
                 print(f"{num_actual} is in half {half}")
                 print(f"{num_actual} is in third {third}")
+            print()
             #Put win conditions here
         case "balance":
             print(f"Current balance: ${balance:.2f}")
@@ -184,24 +187,26 @@ while True:
 ---
 Odd/Even: Bet on every odd number or every even number, 1 to 1 payout
 Red/Black: Bet on every red number or every black number, 1 to 1 payout
-Row: Bet on the entire first, second, or third horizontal row on the table, 2 to 1 payout
-Half: Bet on the entire first or second vertical half of the table, 1 to 1 payout
+Column: Bet on the entire first, second, or third horizontal row on the table, 2 to 1 payout
+1-18 or 19-36: Bet on the entire first or second vertical half of the table, 1 to 1 payout
 Dozen: Bet on the entire first, second, or third vertical third of the table, 2 to 1 payout
-Straight up: Bet on one number on the table, 36 to 1 payout. Multiple numbers can be bet on at the same time
+Single: Bet on one number on the table, 36 to 1 payout. Multiple numbers can be bet on at the same time!
 
 The table layout is as follows:
-3r  6b  9r  12r  15b  18r  21r  24b  27r  30r  33b  36r   row 3
-2b  5r  8b  11b  14r  17b  20b  23r  26b  29b  32r  35b   row 2
-1r  4b  7r  10b  13b  16r  19r  22b  25r  28b  31b  34r   row 1
+3r  6b  9r  12r  15b  18r  21r  24b  27r  30r  33b  36r   column 3
+2b  5r  8b  11b  14r  17b  20b  23r  26b  29b  32r  35b   column 2
+1r  4b  7r  10b  13b  16r  19r  22b  25r  28b  31b  34r   column 1
      third 1    |     third 2      |      third 3       |
-          half 1          |          half 2             |
+         half 1           |          half 2             |
 
 The table also includes a green 0 and a green 00, which will each have a 36 to 1 payout
 ---""")
+        case "save":
+            print() #Do this
         case "quit":
             print(f"Final money amount: ${balance:.2f}")
             print("Bye!")
             break
         case _:
-            print(f"{inp} is not a valid command")
-    print("Type \"bet\" to continue playing, \"balance\" to view current balance, \"help\" for info on betting, or \"quit\" to quit")
+            print(f"\"{inp}\" is not a valid command")
+    print("Type \"bet\" to continue playing, \"balance\" to view current balance, \"help\" for info on betting, \"save\" to save or restore data, or \"quit\" to quit")
